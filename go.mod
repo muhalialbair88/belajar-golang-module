e3a8e3d0bf0c8b3cf05b5d720367f1fd87512ca0
muhalialbair88 <muhalialbair88@gmail.com>
2022-02-20 12:05:31 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/muhalialbair88/belajar-golang-module
+// module github.com/muhalialbair88/belajar-golang-module
+module github.com/muhalialbair88/belajar-golang-module/v2
+
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,16 @@
 package belajar_golang_module
 
-func SayHello() string  {
-	return "Hello World"
+// v1.0.0
+// func SayHello() string  {
+// 	return "Hello"
+// }
+
+// v1.5.0
+// func SayHello() string  {
+// 	return "Hello World"
+// }
+
+// v1.5.0
+func SayHello(name string) string  {
+	return "Hello" + name
 }
